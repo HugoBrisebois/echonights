40daa80cf383f3055c52e482e2cb4eb789ba4757
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2025-07-16 18:00:31 -0400

    finished the main menu with functional  buttons although has a bug in the code that doesn't allow the gamepad to press and then go the the next scene

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_1.tscn")
+
+
+func _on_tutorial_pressed() -> void:
+	get_tree().change_scene_to_file("res://tutorial.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
