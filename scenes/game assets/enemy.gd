7a53a407ebe0167871e5011904028b2161202745
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2025-08-07 11:54:07 -0400

    added soundeffects

--- scenes/game assets/enemy.gd
+++ scenes/game assets/enemy.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
 @onready var gamemanager: Node = %gamemanager
+@onready var sfx_death: AudioStreamPlayer = %sfx_death
+@onready var enemy_animation: AnimatedSprite2D = $enemy_animation
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +15,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func play_death():
+	sfx_death.play()
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if (body.name == "player"):
 		var y_delta = position.y - body.position.y
@@ -19,6 +25,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		print(y_delta)
 		if (y_delta > 18):
 			print("destroy enemy")
+			enemy_animation.animation= "death"
 			queue_free()
 			body.jump()
 		else:
@@ -28,3 +35,4 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 				body.jump_side(500)
 			else:
 				body.jump_side(-500)
+			play_death()
